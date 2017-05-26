a92161b721844098e4e55fa8986df542876642e4
Bruce McLean <mcleanb@photo-nomad.net>
2017-05-26 09:43:41 -0400

    TestConversions - added more test cases.  Changed to check job database for job status. Added timeout for job start.

--- tests/integration_tests/test_conversion.py
+++ tests/integration_tests/test_conversion.py
@@ -11,6 +11,8 @@ import shutil
 
 import time
 
+from manager.manager import TxManager
+
 from general_tools.file_utils import unzip
 
 from aws_tools.s3_handler import S3Handler
@@ -23,11 +25,66 @@ COMMIT_LENGTH = 40
 
 class TestConversions(TestCase):
 
+    def setUp(self):
+        self.api_url = 'https://dev-api.door43.org'
+        self.pre_convert_bucket = 'dev-tx-webhook-client'
+        self.gogs_url = 'https://git.door43.org'
+        self.cdn_bucket = 'dev-cdn.door43.org'
+        self.job_table_name = 'dev-tx-job'
+        self.module_table_name = 'dev-tx-module'
+        self.cdn_url = 'https://dev-cdn.door43.org'
+
     def tearDown(self):
         """Runs after each test."""
         # delete temp files
         if hasattr(self, 'temp_dir') and os.path.isdir(self.temp_dir):
             shutil.rmtree(self.temp_dir, ignore_errors=True)
+
+    def test_usfm_mat_bundle(self):
+        # given
+        if not self.doWeWantToRunTest(): return # skip test if integration test not enabled
+        baseUrl = "https://git.door43.org"
+        user = "vedhanthavijay"
+        repo = "kpb_mat_text_udb"
+        expectedOutputName = "41-MAT"
+
+        # when
+        build_log_json, commitID, commitPath, commitSha, success = self.doConversionForRepo(baseUrl, user, repo)
+
+        # then
+        self.validateBible(user, repo, success, build_log_json, commitID, commitSha, commitPath, expectedOutputName)
+
+    def test_usfm_acts_bundle(self):
+        # given
+        if not self.doWeWantToRunTest(): return # skip test if integration test not enabled
+        baseUrl = "https://git.door43.org"
+        user = "lversaw"
+        repo = "awa_act_text_reg"
+        expectedOutputName = "45-ACT"
+
+        # when
+        build_log_json, commitID, commitPath, commitSha, success = self.doConversionForRepo(baseUrl, user, repo)
+
+        # then
+        self.validateBible(user, repo, success, build_log_json, commitID, commitSha, commitPath, expectedOutputName)
+
+    def test_usfm_acts2_bundle(self):
+        # given
+        if not self.doWeWantToRunTest(): return # skip test if integration test not enabled
+        baseUrl = "https://git.door43.org"
+        user = "deva"
+        repo = "kan-x-aruvu_act_text_udb"
+        expectedOutputName = "45-ACT"
+
+        # when
+        build_log_json, commitID, commitPath, commitSha, success = self.doConversionForRepo(baseUrl, user, repo)
+
+        # then
+        self.validateBible(user, repo, success, build_log_json, commitID, commitSha, commitPath, expectedOutputName)
+
+    ##
+    ## handlers
+    ##
 
     def doWeWantToRunTest(self):
         test = os.environ.get('TEST_DEPLOYED',"")
@@ -39,42 +96,20 @@ class TestConversions(TestCase):
             self.assertTrue(len(gogsUserToken) > 0, "GOGS_USER_TOKEN is missing in environment")
         return doTest
 
-    def test_usfm_bundle(self):
-        if not self.doWeWantToRunTest():
-            return # skip test if not running master branch
-
-        # given
-        self.api_url = 'https://dev-api.door43.org'
-        self.pre_convert_bucket = 'dev-tx-webhook-client'
-        self.gogs_url = 'https://git.door43.org'
-        self.cdn_bucket = 'dev-cdn.door43.org'
-        baseUrl = "https://git.door43.org"
-        user = "deva"
-        repo = "kan-x-aruvu_act_text_udb"
-        expectedOutputName = "45-ACT"
-
-        # when
-        build_log_json, commitID, commitPath, commitSha, success = self.doConversionForRepo(baseUrl, user, repo)
-
-        # then
+    def validateBible(self, user, repo, success, build_log_json, commitID, commitSha, commitPath, expectedOutputName):
         self.assertTrue(len(build_log_json) > 0)
         self.assertTrue(len(build_log_json['errors']) == 0, "Found errors: " + str(build_log_json['errors']))
         self.assertTrue(success)
-
         self.temp_dir = tempfile.mkdtemp(prefix='testing_')
         self.downloadAndCheckZipFile(self.s3_handler, expectedOutputName + ".usfm", self.getPreconvertS3Key(commitSha),
                                      "preconvert")
-
         # this gets deleted after conversion:
         # self.downloadAndCheckZipFile(self.cdn_handler, expectedOutputName + ".html", self.getTxOutputS3Key(commitID), "output")
-
-        self.checkDestinationFiles(self.cdn_handler, expectedOutputName + ".html", self.getDestinationS3Key(commitSha, repo, user))
-
+        self.checkDestinationFiles(self.cdn_handler, expectedOutputName + ".html",
+                                   self.getDestinationS3Key(commitSha, repo, user))
         self.assertEqual(len(commitID), COMMIT_LENGTH)
         self.assertIsNotNone(commitSha)
         self.assertIsNotNone(commitPath)
-
-    ## handlers
 
     def downloadAndCheckZipFile(self, handler, expectedOutputFile, key, type):
         zipPath = os.path.join(self.temp_dir, type + ".zip")
@@ -127,8 +162,8 @@ class TestConversions(TestCase):
             self.cdn_handler.delete_file(txOutput_key, catch_exception=True)
 
     def getTxOutputS3Key(self, commitID):
-        destination_key = 'tx/job/{0}.zip'.format(commitID)
-        return destination_key
+        output_key = 'tx/job/{0}.zip'.format(commitID)
+        return output_key
 
     def getDestinationS3Key(self, commitSha, repo, user):
         destination_key = 'u/{0}/{1}/{2}'.format(user, repo, commitSha)
@@ -177,35 +212,55 @@ class TestConversions(TestCase):
             'commit_data': webhookData
         }
         try:
-            ClientWebhook(**env_vars).process_webhook()
+            build_log_json = ClientWebhook(**env_vars).process_webhook()
         except Exception as e:
             message = "Exception: " + str(e)
             print(message)
             return None, False
 
-        build_log_json, success = self.pollUntilJobFinished(commitSha, repo, user)
-        return build_log_json, success
-
-    def pollUntilJobFinished(self, commitSha, repo, user):
-        build_log_json = None
-        success = False
-        pollingTimeout = 5 * 60 # poll for up to 5 minutes for job to complete or error
-        sleepInterval = 5 # how often to check for completion
-        for i in range(0, pollingTimeout / sleepInterval):
-            build_log_json = self.getJsonFile(commitSha, 'build_log.json', repo, user)
-            self.assertTrue(len(build_log_json) > 0)
-            if len(build_log_json['errors']) > 0:
-                print("Found errors: " + str(build_log_json['errors']))
-                break
-            if build_log_json['ended_at'] != None:
-                success = True
-                break
-            print("status at " + str(i) + ":\n" + str(build_log_json))
-            time.sleep(sleepInterval)
-
+        success = self.pollUntilJobFinished(build_log_json['job_id'])
+        build_log_json = self.getJsonFile(commitSha, 'build_log.json', repo, user)
         if build_log_json != None:
             print("Final results:\n" + str(build_log_json))
         return build_log_json, success
+
+    def pollUntilJobFinished(self, job_id):
+        success = False
+
+        env_vars = {
+            'api_url': self.api_url,
+            'gogs_url': self.gogs_url,
+            'cdn_url': self.cdn_url,
+            'job_table_name':  self.job_table_name,
+            'module_table_name': self.module_table_name,
+            'cdn_bucket': self.cdn_bucket
+        }
+        tx_manager = TxManager(**env_vars)
+
+        pollingTimeout = 5 * 60 # poll for up to 5 minutes for job to complete or error
+        sleepInterval = 5 # how often to check for completion
+        startMaxWaitCount = 30 / sleepInterval # maximum count to wait for conversion to start (sec/interval)
+        for i in range(0, pollingTimeout / sleepInterval):
+            job = tx_manager.get_job(job_id)
+            self.assertIsNotNone(job)
+            print("job status at " + str(i) + ":\n" + str(job.log))
+
+            if len(job.errors) > 0:
+                print("Found errors: " + str(job.errors))
+                break
+
+            if job.ended_at != None:
+                success = True
+                break
+
+            if (i > startMaxWaitCount) and (job.started_at == None):
+                success = False
+                print("Conversion Failed to start")
+                break
+
+            time.sleep(sleepInterval)
+
+        return success
 
     def getJsonFile(self, commitSha, file, repo, user):
         key = 'u/{0}/{1}/{2}/{3}'.format(user, repo, commitSha, file)
